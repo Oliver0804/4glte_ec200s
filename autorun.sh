28be02b5a021b0cb1802674be7381f83f24a4d76
oliver0804 <icetzsr@gmail.com>
2019-02-14 18:15:30 +0800

    fixed route ppp0

--- autorun.sh
+++ autorun.sh
@@ -4,3 +4,7 @@ sleep 1
 sudo sh -c 'echo "2c7c 6002" > /sys/bus/usb-serial/drivers/option1/new_id'
 sleep 1
 sudo /usr/sbin/pppd call quectel-ppp &
+sleep 1
+sudo route del default
+sleep 1
+sudo route add default ppp0
